f58cf09a139774aa4c364d6979ddab9d9559494b
sokolyaka <sokolov.ob@gmail.com>
2018-08-01 22:20:41 +0300

    copied isAvailable() implementation from ImplementInterface source code

--- src/main/java/com/sokolov/decoratorsIdeaPlugin/dialog/DecoratorDialogAction.java
+++ src/main/java/com/sokolov/decoratorsIdeaPlugin/dialog/DecoratorDialogAction.java
@@ -1,16 +1,22 @@
 package com.sokolov.decoratorsIdeaPlugin.dialog;
 
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightNamesUtil;
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction;
-import com.intellij.ide.highlighter.JavaFileType;
+import com.intellij.ide.scratch.ScratchFileType;
+import com.intellij.lang.java.JavaLanguage;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileFactory;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.*;
 import com.intellij.psi.impl.file.PsiDirectoryFactory;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtilCore;
 import com.intellij.util.IncorrectOperationException;
 import com.sokolov.decoratorsIdeaPlugin.dialog.domain.addDecoratorToProject.AddDecoratorToPsiDirectoryUseCase;
 import com.sokolov.decoratorsIdeaPlugin.dialog.domain.addDecoratorToProject.InWriteActionAddDecoratorToProjectUseCase;
@@ -41,10 +47,58 @@ public class DecoratorDialogAction extends BaseIntentionAction {
         return "Generate decorator";
     }
 
+    private static boolean isSupportedLanguage(PsiClass aClass) {
+        return aClass.getLanguage() == JavaLanguage.INSTANCE;
+    }
+
+    private static boolean shouldCreateInnerClass(PsiClass psiClass) {
+        return psiClass.hasModifierProperty(PsiModifier.PRIVATE) && psiClass.getContainingClass() != null;
+    }
+
     @Override
     public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
-        return
-                file.getFileType().equals(JavaFileType.INSTANCE);
+        final CaretModel caretModel = editor.getCaretModel();
+        final int position = caretModel.getOffset();
+        PsiElement element = file.findElementAt(position);
+        PsiClass psiClass = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+        if (psiClass == null || psiClass.isAnnotationType() || psiClass.isEnum() || psiClass instanceof PsiAnonymousClass ||
+                psiClass.hasModifierProperty(PsiModifier.FINAL)) {
+            return false;
+        }
+        VirtualFile virtualFile = PsiUtilCore.getVirtualFile(psiClass);
+        if (virtualFile == null || virtualFile.getFileType() == ScratchFileType.INSTANCE) {
+            return false;
+        }
+        if (!isSupportedLanguage(psiClass)) return false;
+        final PsiMethod[] constructors = psiClass.getConstructors();
+        if (constructors.length > 0) {
+            boolean hasNonPrivateConstructor = false;
+            for (PsiMethod constructor : constructors) {
+                if (!constructor.hasModifierProperty(PsiModifier.PRIVATE)) {
+                    hasNonPrivateConstructor = true;
+                    break;
+                }
+            }
+            if (!hasNonPrivateConstructor) return false;
+        }
+        PsiElement lBrace = psiClass.getLBrace();
+        if (lBrace == null) return false;
+        if (element.getTextOffset() >= lBrace.getTextOffset()) return false;
+
+        TextRange declarationRange = HighlightNamesUtil.getClassDeclarationTextRange(psiClass);
+        final TextRange elementTextRange = element.getTextRange();
+        if (!declarationRange.contains(elementTextRange)) {
+            if (!(element instanceof PsiWhiteSpace) || (declarationRange.getStartOffset() != elementTextRange.getEndOffset() &&
+                    declarationRange.getEndOffset() != elementTextRange.getStartOffset())) {
+                return false;
+            }
+        }
+
+        if (shouldCreateInnerClass(psiClass) && !file.getManager().isInProject(file)) {
+            return false;
+        }
+
+        return true;
     }
 
     @Override
